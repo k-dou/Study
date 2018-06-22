a87f94f703eb7c1235434b23f59cb80cb31ededb
k-dou <kyangmail@foxmail.com>
2018-06-22 14:38:27 +0800

    add path

---
+++ symfony2/sf2_test/path/src/Scourgen/WebBundle/Entity/Author.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/6/22
+ * Time: 10:02
+ */
---
+++ symfony2/sf2_test/path/src/Scourgen/WebBundle/ScourgenWebBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Scourgen\WebBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ScourgenWebBundle extends Bundle
+{
+}
